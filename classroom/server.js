8b991d55af8e84e13856c93f492f44969f506bdd
animeshgupta2023 <animesh1cs1h@gmail.com>
2026-08-06 22:28:10 +0530

    phase 2 part b cookies

--- classroom/server.js
+++ classroom/server.js
@@ -2,8 +2,33 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const cookieParser = require("cookie-parser");
+
+app.use(cookieParser("secretcode"));
+
+app.get("/getsignedcookie", (req, res)=>{
+    res.cookie("made-in", "india", {signed: true});
+    res.send("sent signed cookie");
+});
+
+app.get("/verify", (req, res)=>{
+    res.send(req.signedCookies);
+});
+
+// sending cookies
+app.get("/getcookies", (req, res)=>{
+    res.cookie("greet", "namaste");
+    res.cookie("madeIn", "India");
+    res.send("sent you some cookies");
+}); 
+
+app.get("/greet", (req, res)=>{
+    let {name="Anonymous"} = req.cookies;
+    res.send(`hi ${name}`);
+});
 
 app.get("/", (req, res)=>{
+    console.log(req.cookies);
     res.send("Hi, I am root!");
 });
 
